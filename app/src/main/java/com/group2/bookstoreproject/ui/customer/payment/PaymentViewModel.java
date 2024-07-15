90a985a4bcf037d2a542893ba8c4cd1e5fb2cdcb
NguyenChienThang <chienthang014@gmail.com>
2024-07-15 12:49:50 +0700

    update payment

--- app/src/main/java/com/group2/bookstoreproject/ui/customer/payment/PaymentViewModel.java
+++ app/src/main/java/com/group2/bookstoreproject/ui/customer/payment/PaymentViewModel.java
@@ -1,14 +1,21 @@
 package com.group2.bookstoreproject.ui.customer.payment;
 
+import static com.group2.bookstoreproject.ui.common.chat.ChatViewModel.TAG;
+
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 import com.group2.bookstoreproject.base.BaseViewModel;
 import com.group2.bookstoreproject.data.model.CartItem;
+import com.group2.bookstoreproject.data.model.Order;
 import com.group2.bookstoreproject.data.model.User;
 import com.group2.bookstoreproject.data.repository.CartItemRepository;
+import com.group2.bookstoreproject.data.repository.OrderRepository;
 import com.group2.bookstoreproject.data.repository.ProfileRepository;
 import com.group2.bookstoreproject.data.repositoryImpl.CartItemRepositoryImpl;
+import com.group2.bookstoreproject.data.repositoryImpl.OrderRepositoryImpl;
 import com.group2.bookstoreproject.data.repositoryImpl.ProfileRepositoryImpl;
 
 import java.util.List;
@@ -18,10 +25,14 @@ public class PaymentViewModel extends BaseViewModel {
     private final MutableLiveData<List<CartItem>> cartItemsLiveData = new MutableLiveData<>();
     private final MutableLiveData<User> userLiveData = new MutableLiveData<>();
     private final ProfileRepository profileRepository;
+    private final OrderRepository orderRepository;
+
 
     public PaymentViewModel() {
         cartRepository = new CartItemRepositoryImpl();
         profileRepository = new ProfileRepositoryImpl();
+        orderRepository = new OrderRepositoryImpl();
+
     }
 
     public LiveData<List<CartItem>> getCartItemsLiveData() {
@@ -50,6 +61,16 @@ public class PaymentViewModel extends BaseViewModel {
                 userLiveData.setValue(user);
             } else {
                 // handle error here if needed
+            }
+        });
+    }
+
+    public void addOrder(Order order) {
+        orderRepository.upsert(order.getOrderId(), order, task -> {
+            if (task.isSuccessful()) {
+                Log.d(TAG, "Order added successfully");
+            } else {
+                Log.e(TAG, "Error adding Order", task.getException());
             }
         });
     }
